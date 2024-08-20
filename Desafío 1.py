32ca574317187d30582baacd6040d5c8ecf6033b
nvera <nvera@uade.edu.ar>
2024-08-20 09:47:30 -0300

    Desafio 1 modificado

--- Desafío 1.py
+++ Desafío 1.py
@@ -21,14 +21,33 @@ Instrucciones:
         o calcular_promedio_estudiantes(matriz): Calcular y mostrar el promedio de calificaciones para cada estudiante.
         o calcular_promedio_materias(matriz): Calcular y mostrar el promedio de calificaciones para cada materia."""
 import random
-   
-def mostrar_matriz (matriz, estudiantes, materias):
-    filas = len(matriz) #cantidad de filas
-    columnas= len(matriz[0]) #cantidad de columnas
+def crear_matriz (n,m):
+    """
+    pre: recibe n y m. Donde n: filas y M: columnas.
+    pos: devuelve una matriz de NxM creada con ceros.
+    """
+    return [[0]*m for fil in range (n)]
+
+def llenar_matriz (m):
+    """
+    pre: recibe una matriz ya creada.
+    pos: llena la matriz con valores aleatorios entre 1 y 10    
+    """
+    filas = len(m) #cantidad de filas
+    columnas= len(m[0]) #cantidad de columnas
     for fil in range (filas):
         for col in range (columnas):
-            print("El alumno",estudiantes[fil],"se sacó una nota de",matriz[fil][col],"en",materias[col])        
+            num_aleatorio = random.randint (1,9)
+            m[fil][col]= num_aleatorio
+    return m
+   
+def mostrar_matriz (matriz, estudiantes, materias):
+    print("       " + "  ".join(materias))
     
+    for i, fila in enumerate(matriz):
+        print(f"{estudiantes[i]:<10} " + "  ".join(str(calificacion) for calificacion in fila))
+        
+"""    
 def calcular_promedio_estudiantes(matriz,estudiantes,materias):
     filas = len(matriz) #cantidad de filas
     columnas= len(matriz[0]) #cantidad de columnas
@@ -49,37 +68,17 @@ def calcular_promedio_materia(matriz,estudiantes,materias):
             suma += matriz[fil][col]
         prom = suma / len(estudiantes)
         print("El promedio de",materias[col],"fue de ",prom)
-
-
-def crear_matriz (n,m):
-    """
-    pre: recibe n y m. Donde n: filas y M: columnas.
-    pos: devuelve una matriz de NxM creada con ceros.
-    """
-    return [[0]*m for fil in range (n)]
-
-def llenar_matriz (m):
-    """
-    matriz regular
-    pre: recibe una matriz ya creada.
-    pos: llena la matriz con valores aleatorios entre 1 y 10    
-    """
-    filas = len(m) #cantidad de filas
-    columnas= len(m[0]) #cantidad de columnas
-    for fil in range (filas):
-        for col in range (columnas):
-            num_aleatorio = random.randint (1,9)
-            m[fil][col]= num_aleatorio
-    return m
-
+"""
+print
 lista_estudiantes = ["Juan","Ana","Nicolas","Martin","Ariel"] 
-lista_materias = ["Matemáticas", "Historia", "Ciencias", "Inglés","C1","C2","C3"] 
+lista_materias = ["M", "H", "C", "I","B","T","G"] 
 a=crear_matriz(len(lista_estudiantes),len(lista_materias))
 b=llenar_matriz(a)
 print()
 mostrar_matriz(b,lista_estudiantes,lista_materias)
-
+"""
 print()
 calcular_promedio_estudiantes(b,lista_estudiantes,lista_materias)
 print()
 calcular_promedio_materia(b,lista_estudiantes,lista_materias)
+"""
